16019d9fbdd4d756648fccb974ce8f42156490af
SuphawitCE <b4nknaja@hotmail.com>
2022-12-23 15:15:28 +0700

    feat(init): setup project with basic api and structure

---
+++ README.md
@@ -0,0 +1,24 @@
+# Node.js REST API with TypeScript
+
+The main purpose of this repository is to demonstrate a basic working Node.js API Server with Express and TypeScript.
+
+## Prerequisite
+- Node.js runtime version >= 14
+
+## Getting Started
+
+Install node modules
+```
+cd node-api-type-script
+npm i
+```
+
+Build TypeScript or compile code to JavaScript
+```
+npm run build-ts
+```
+
+Run server
+```
+npm start
+```
---
+++ src/routes/todos.ts
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+
+import { Todo } from '../models/todo';
+
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
+let todos: Todo[] = [];
+
+const router = Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({ todos });
+});
+
+router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody;
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: body.text
+  };
+
+  todos.push(newTodo);
+
+  res.status(201).json({ message: 'Added todo', todo: newTodo, todos });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  const todoId = params.todoId;
+  const body = req.body as RequestBody;
+
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = {
+      id: todos[todoIndex].id,
+      text: body.text
+    };
+
+    return res.status(200).json({ message: 'Updated todo', todos });
+  }
+
+  res.status(404).json({ message: 'Could not find todo for this ID' });
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
+
+  res.status(200).json({ message: 'Deleted todo', todos });
+});
+
+export default router;
